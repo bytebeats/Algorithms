3c8b3b39da29f8dc32471bf305479c71a7fd4e50
bytebeats <happychinapc@gmail.com>
2020-05-22 23:22:00 +0800

    Problems: 面试题16.05.

--- src/me/bytebeats/algorithms/kt/Solution6.kt
+++ src/me/bytebeats/algorithms/kt/Solution6.kt
@@ -939,4 +939,15 @@ class Solution6 {
         }
         return ans
     }
+
+    fun trailingZeroes(n: Int): Int {//面试题16.05
+        var ans = 0
+        var k = n
+        while (k > 1) {
+            k /= 5
+            ans++
+        }
+        return ans
+
+    }
 }
