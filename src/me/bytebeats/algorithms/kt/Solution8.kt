b45cc4df3deb6977e69e734077046ddd00c5d332
panchen <panchen@tigerbrokers.com>
2020-06-18 18:37:51 +0800

    Problems: 1028.

--- src/me/bytebeats/algorithms/kt/Solution8.kt
+++ src/me/bytebeats/algorithms/kt/Solution8.kt
@@ -701,7 +701,37 @@ class Solution8 {
     }
 
     fun recoverFromPreorder(S: String): TreeNode? {//1028
-        return null
+        val path = mutableListOf<TreeNode>()
+        var pos = 0
+        while (pos < S.length) {
+            var level = 0
+            while (S[pos] == '-') {
+                level++
+                pos++
+            }
+            var value = 0
+            while (pos < S.length && S[pos].isDigit()) {
+                value *= 10
+                value += S[pos] - '0'
+                pos++
+            }
+            val node = TreeNode(value)
+            if (level == path.size) {
+                if (path.isNotEmpty()) {
+                    path.last()?.left = node
+                }
+            } else {
+                while (level != path.size) {
+                    path.removeAt(path.lastIndex)
+                }
+                path.last()?.right = node
+            }
+            path.add(node)
+        }
+        while (path.size > 1) {
+            path.removeAt(path.lastIndex)
+        }
+        return path.last()
     }
 
     fun hIndex(citations: IntArray): Int {//274
