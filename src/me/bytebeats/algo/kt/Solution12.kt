153517a36d07ff2b34f94971883304da909b0099
panchen <panchen@tigerbrokers.com>
2021-01-02 19:40:33 +0800

    Problems: 1397.

---
+++ src/me/bytebeats/algo/kt/Solution12.kt
@@ -0,0 +1,53 @@
+package me.bytebeats.algo.kt
+
+/**
+ * @Author bytebeats
+ * @Email <happychinapc@gmail.com>
+ * @Github https://github.com/bytebeats
+ * @Created on 2020/12/14 20:31
+ * @Version 1.0
+ * @Description TO-DO
+ */
+
+class Solution12 {
+    fun findSubArray(arr: IntArray, target: Int): Int {
+        var minL = Int.MAX_VALUE
+        val size = arr.size
+        for (i in 0 until size) {
+            var sum = arr[i]
+            var tmp = 1
+            for (j in i + 1 until size) {
+                if (sum >= target && tmp < minL) {
+                    minL = tmp
+                    break
+                }
+                sum += arr[j]
+                tmp += 1
+            }
+            if (sum >= target && tmp < minL) {
+                minL = tmp
+            }
+        }
+        return minL
+    }
+
+    fun findSubArray1(arr: IntArray, target: Int): Int {
+        var minL = Int.MAX_VALUE
+        var i = 0
+        var j = 0
+        var sum = 0
+        while (j < arr.size) {
+            sum += arr[j]
+            if (sum >= target) {
+                if (j - i + 1 < minL) {
+                    minL = j - i + 1
+                }
+                while (sum >= target) {
+                    sum -= arr[i++]
+                }
+            }
+            j++
+        }
+        return minL
+    }
+}
